bb687467ddb074ac9e6a30b9eaa2bfaabe3499eb
Luis Gustavo <python15772@gmail.com>
2023-05-26 15:50:30 -0300

    Estou adicionando o último exercício de dicionario do curso em vídeo

--- exercicios-python/ex0093.py
+++ exercicios-python/ex0093.py
@@ -2,26 +2,82 @@
  O programa vai ler o nome do jogador e quantas partidas ele jogou. Depois vai ler a quantidade de gols feitos em cada partida.
  No final, tudo isso será guardado em um dicionário, incluindo o total de gols feitos durante o campeonato.
 '''
-gols =[]
-cont =0
-jogador = {}
-jogador['nome'] = str(input('Digite o nome do jogador: '.strip()))
-jogador['partidas'] = int(input(f'Digite quantas partidas o \033[1;31m{jogador["nome"]}\033[m jogou:  '))
-while cont < jogador["partidas"]:
-    cont +=1
-    gols.append(int(input(f'Quantos gols na {cont}º partida : ')))
+from time import sleep
+'''Exercício Python 095: Aprimore o desafio 93 para que ele funcione com vários jogadores, 
+incluindo um sistema de visualização de detalhes do aproveitamento de cada jogador.'''
+estatis = []
+while True:
+    cont =0
+    gols =[]
+    jogador ={}
+    if len(estatis) ==0:
+        jogador['cod'] = 0
+    if len(estatis) > 0:
+        jogador['cod'] = estatis[len(estatis)-1]['cod'] +1
+    jogador['nome'] = str(input('Digite o nome do jogador: '.strip()))
+    jogador['partidas'] =int(input(f'Quantas partidas o {jogador["nome"]} jogou: '.strip()))
+    cont =0
+    while cont< jogador['partidas']:
+        cont +=1
+        gols.append(int(input(f'Quantos gols ele fez na {cont}º partida')))
+    jogador['gols'] =gols[:]
+    gols.clear()
+    estatis.append(jogador.copy())
+    op =str(input('Deseja continuar cadastrando-[S/N]: ')).upper().strip()[0]
+    if op == 'N':
+        jogador.clear()
+        break
+    if op =='S':
+        pass
+    if op != 'S' and op!='N':
+        print('OPÇÃO INVÁLIDA')
+        print('FECHRANDO...')
+        sleep(2)
+#Beleza, o programa roda os dados dos jogadores em um dicionário e guarda ele em uma lista(estatis)
+#incluindo um sistema de visualização de detalhes do aproveitamento de cada jogador.'''
+print('{:<5s}{:<20s}{:<10s}{:<100s}'.format("COD", "NOME", "GOLS", "TOTAL"))
+print('_______________________________________________________________')
 
-jogador['gols'] = gols[:]
-jogador['total'] = sum(gols)
-print('-='*30)
-print(jogador)
-print('-='*30)
-for k,v in jogador.items():
-    print(f'\033[1;33m{k}\033[1;34m tem o valor\033[m \033[1;33m{v} \033[m ')
-print('-='*30)
-print(f'O {jogador["nome"]} tem {jogador["partidas"]} partidas')
-for k,c in enumerate(gols):
-    print(f'Na {k+1}º partida, fez {c}gols')
+len_de_veri = 0
+while len(estatis) > len_de_veri:
+    cod = str(estatis[len_de_veri]["cod"])
+    nome = estatis[len_de_veri]["nome"]
+    gols = str(estatis[len_de_veri]["gols"])
+    total = str(sum(estatis[len_de_veri]["gols"]))
+
+    print('{:<5s}{:<20s}{:<10s}{:100s}'.format(cod, nome, gols, total))
+    len_de_veri += 1
+op =0
+umaitera = 0
+cont2 =0
+while True:
+    verificar_se_encontrou_jogador = 0
+    cont2 =0
+    op = int(input('Digite o cod para analisar o jogador(999 Encerra o programa): '.strip()))
+    if op ==999:
+        break
+    else:
+        n_index_da_lista = len(estatis) -1
+        for k,c in enumerate(estatis):
+            if estatis[k]['cod']==op:
+                for g in c['gols']:
+                    cont2 +=1
+                    print(f"O jogador {estatis[k]['nome']},na sua {cont2}º partida, fez {g} gols")
+                    sleep(0.7)
+                    verificar_se_encontrou_jogador =1
+            if k == n_index_da_lista and verificar_se_encontrou_jogador == 0:
+                print('Erro! Não existe Jogador com este cod')
+
+
+
+
+
+
+
+
+
+
+
 
 
 
