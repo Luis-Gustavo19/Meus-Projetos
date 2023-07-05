e70fc153a536f898ab9a972eaa54b34aa72a2c12
Luis Gustavo <129898260+Luis-Gustavo19@users.noreply.github.com>
2023-07-05 14:59:43 -0300

    Update ex0076.py
    atualização de coment

--- exercicios-python/ex0076.py
+++ exercicios-python/ex0076.py
@@ -3,7 +3,6 @@
 #TUPLA COM NOME DE PRODUTO ESEU PREÇO
 preco = ('computador', 2800.50,'Teclado',60.00,'Monitor',160.00, 'Play Station 3',1200.00,'Mouse',55.00)
 cont1 = -1
-#tô com dificuldades p fzr esse, porém conseguir
 print('=' * 50)
 print('LISTAGEM DE PREÇO')
 print('=' * 50)
