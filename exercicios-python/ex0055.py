c60a61c42bc404256feaad1bd6e83e534e1f6325
Luis Gustavo <129898260+Luis-Gustavo19@users.noreply.github.com>
2023-07-05 15:06:13 -0300

    Update ex0055.py
    coment edition do ex 55

--- exercicios-python/ex0055.py
+++ exercicios-python/ex0055.py
@@ -1,5 +1,4 @@
 '''Exercício Python 55: Faça um programa que leia o peso de cinco pessoas. No final, mostre qual foi o maior e o menor peso lidos'''
-#não entendi de começo, mas vendo a resolução, entendi.
 '''maior = 0
 menor = 0
 for c in range(1,6):
