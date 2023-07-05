6f648ac364d880a7a1174adabc579c7ea4e39d0e
Luis Gustavo <129898260+Luis-Gustavo19@users.noreply.github.com>
2023-07-05 15:01:56 -0300

    Update ex0072.py
    atualização de coment

--- exercicios-python/ex0072.py
+++ exercicios-python/ex0072.py
@@ -1,6 +1,5 @@
 '''Exercício Python 72: Crie um programa que tenha uma dupla totalmente preenchida com uma contagem por extenso,
  de zero até vinte. Seu programa deverá ler um número pelo teclado (entre 0 e 20) e mostrá-lo por extenso.'''
-#o meu funcionou mas n entendi kkk
 tupla =('zero','um','dois','três','Quatro','cinco','seis','sete','oito','nove','dez','onze','doze','treze','quatorze','quinze','dezesseis','dezessete','dezoito','dezenove','vinte')
 '''n = int(input('Digite um número entre 0 e 20: '))
 while n < 0 or n >20:
