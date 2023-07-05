1e887d0962a146ae6c75299f9844c8b0119bbc5c
Luis Gustavo <129898260+Luis-Gustavo19@users.noreply.github.com>
2023-07-05 15:04:57 -0300

    Update ex0060.py
    coment edition

--- exercicios-python/ex0060.py
+++ exercicios-python/ex0060.py
@@ -1,6 +1,5 @@
 '''Exercício Python 060: Faça um programa que leia um número qualquer e mostre o seu fatorial. Exemplo:
 5! = 5 x 4 x 3 x 2 x 1 = 120'''
-#difícil p caceteeeeeeeeeeeeeeeeeeeeee
 n = int(input('Digite o número para saber seu fatorial:  '))
 fatorial = 1
 while n > 0:
