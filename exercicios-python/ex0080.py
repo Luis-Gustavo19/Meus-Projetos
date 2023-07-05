c92590950bf03127332104d83a00f900895c67ac
Luis Gustavo <129898260+Luis-Gustavo19@users.noreply.github.com>
2023-07-05 14:58:23 -0300

    Update ex0080.py
    atualização de comentario

--- exercicios-python/ex0080.py
+++ exercicios-python/ex0080.py
@@ -1,7 +1,6 @@
 '''Exercício Python 080: Crie um programa onde o usuário possa digitar cinco valores numéricos e cadastre-os em uma lista,
 já na posição correta de inserção (sem usar o sort()).
 No final, mostre a lista ordenada na tela.'''
-#NÃO ENTENDI NADA NADA NADA NADA NADA DE NADA MSM SOU BURRO
 lista = []
 for c in range(0,5):
     num = int(input('Digite um valor para adicionara a lista: '))
