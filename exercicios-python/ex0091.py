b5a36ee5c36ded943e1b1c5418f0e52963618bc3
Luis Gustavo <129898260+Luis-Gustavo19@users.noreply.github.com>
2023-07-05 14:55:09 -0300

    Update ex0091.py
    atuzalização de comentario

--- exercicios-python/ex0091.py
+++ exercicios-python/ex0091.py
@@ -1,7 +1,6 @@
 '''Exercício Python 091: Crie um programa onde 4 jogadores joguem um dado e tenham resultados aleatórios.
 Guarde esses resultados em um dicionário em Python.
 No final, coloque esse dicionário em ordem, sabendo que o vencedor tirou o maior número no dado.'''
-#NÃO FIZ TENDI ND OLHEI A EXPLICAÇÃO
 import random
 from time import sleep
 from operator import itemgetter
